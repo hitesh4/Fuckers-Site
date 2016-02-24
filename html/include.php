593a882f54457869d824983a795434da4940a52e
hitesh4 <saini.hitesh4@gmail.com>
2016-02-24 19:57:03 +0530

    Navbar made

---
+++ html/include.php
@@ -0,0 +1,9 @@
+<!-- CSS FILES -->
+
+<link rel="stylesheet" type="text/css" href="../assets/materialize/css/materialize.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<link rel="stylesheet" type="text/css" href="../assets/animate.css">
+
+
+<!-- JS FILES -->
+<script type="text/javascript" src="../assets/materialize/js/materialize.js"></script>
